24e3fb867947ca5ecf85068c148b1fd21b30ef06
SukaMCD <fabian25march@gmail.com>
2025-03-24 12:27:39 +0700

    Animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
     public Rigidbody2D rb;
+    public Animator anim;
 
     void FixedUpdate ()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        anim.SetFloat("horizontal", horizontal);
+        anim.SetFloat("vertical", vertical);
 
         rb.linearVelocity = new Vector2 (horizontal, vertical) * speed;
     }
