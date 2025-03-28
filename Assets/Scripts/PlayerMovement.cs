c1d74820889fb356f87ed1f0064f0a9d337d7128
SukaMCD <fabian25march@gmail.com>
2025-03-28 22:26:56 +0700

    Tileset

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
+    public int facingDirection = 1;
     public Rigidbody2D rb;
     public Animator anim;
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        anim.SetFloat("horizontal", horizontal);
-        anim.SetFloat("vertical", vertical);
+        // Mengatur animasi gerakan
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
 
-        rb.linearVelocity = new Vector2 (horizontal, vertical) * speed;
+        // Menggerakkan pemain
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+
+        // Membalik arah berdasarkan input horizontal
+        if ((horizontal > 0 && facingDirection < 0) || (horizontal < 0 && facingDirection > 0))
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
